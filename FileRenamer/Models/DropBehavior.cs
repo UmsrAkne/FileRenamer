51db19862b1bdc6242af5c314d9d587fca517572
Akn <sibata486@gmail.com>
2021-01-30 20:49:10 +0900

    init

---
+++ FileRenamer/Models/DropBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace FileRenamer.Models {
+    class DropBehavior : Behavior<ListView>{
+
+        protected override void OnAttached() {
+            base.OnAttached();
+
+            // ファイルをドラッグしてきて、コントロール上に乗せた際の処理
+            this.AssociatedObject.PreviewDragOver += AssociatedObject_PreviewDragOver;
+
+            // ファイルをドロップした際の処理
+            this.AssociatedObject.Drop += AssociatedObject_Drop;
+        }
+
+        private void AssociatedObject_Drop(object sender, DragEventArgs e) {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach(string uriString in files) {
+                System.Diagnostics.Debug.WriteLine(uriString);
+            }
+        }
+
+        private void AssociatedObject_PreviewDragOver(object sender, System.Windows.DragEventArgs e) {
+            e.Effects = DragDropEffects.Copy;
+            e.Handled = e.Data.GetDataPresent(DataFormats.FileDrop);
+        }
+
+        protected override void OnDetaching() {
+            base.OnDetaching();
+            this.AssociatedObject.PreviewDragOver -= AssociatedObject_PreviewDragOver;
+            this.AssociatedObject.Drop -= AssociatedObject_Drop;
+        }
+    }
+}
