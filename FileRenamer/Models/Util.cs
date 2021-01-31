aac4442a51e3c84f40271abd11ddf314c33ddfda
Akn <sibata486@gmail.com>
2021-01-31 17:04:07 +0900

    クラス追加 / 重複するファイル名を検索する機能をもつ Util クラスを追加

---
+++ FileRenamer/Models/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRenamer.Models {
+    public class Util {
+
+        /// <summary>
+        /// 入力されたリスト内の重複する文字列を返却します。
+        /// </summary>
+        /// <param name="names"></param>
+        /// <returns>重複が存在した場合はその文字列を、存在しない場合は空文字を返します。</returns>
+        public string findDuplicateName(List<string> names) {
+            HashSet<String> nameSet = new HashSet<String>();
+            foreach(string name in names) {
+                if (nameSet.Add(name) == false) {
+                    return name;
+                }
+            }
+
+            return "";
+        }
+    }
+}
