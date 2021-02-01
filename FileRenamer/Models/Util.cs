cabdc510a8e6dd8b916e982736700bb667ddae6f
Akn <sibata486@gmail.com>
2021-02-01 15:40:05 +0900

    Util.findDuplicateName の仕様を変更。返却型を文字列からリストに変更
    常に文字列を返すより、リストで返したほうが返された値の意味が明確であるため変更

--- FileRenamer/Models/Util.cs
+++ FileRenamer/Models/Util.cs
@@ -8,19 +8,20 @@ namespace FileRenamer.Models {
     public class Util {
 
         /// <summary>
-        /// 入力されたリスト内の重複する文字列を返却します。
+        /// 入力されたリスト内の重複する文字列のリストを返却します。
         /// </summary>
-        /// <param name="names"></param>
-        /// <returns>重複が存在した場合はその文字列を、存在しない場合は空文字を返します。</returns>
-        public string findDuplicateName(List<string> names) {
+        /// <param name="names">全てのファイル名を含めたリストを入力します。</param>
+        /// <returns>重複が存在した場合はその文字列のリストを、存在しない場合は要素０のリストを返します。</returns>
+        public static List<String> findDuplicateName(List<string> names) {
             HashSet<String> nameSet = new HashSet<String>();
+            var duplicates = new List<String>();
             foreach(string name in names) {
                 if (nameSet.Add(name) == false) {
-                    return name;
+                    duplicates.Add(name);
                 }
             }
 
-            return "";
+            return duplicates;
         }
     }
 }
---
+++ FileRenamerTests/Models/UtilTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileRenamer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRenamer.Models.Tests {
+    [TestClass()]
+    public class UtilTests {
+        [TestMethod()]
+        public void findDuplicateNameTest() {
+            var list = new List<String>(new String[] { "a","b","c","c","d","d"});
+            Assert.AreEqual(Util.findDuplicateName(list)[0], "c");
+            Assert.AreEqual(Util.findDuplicateName(list)[1], "d");
+
+            var listb = new List<String>(new String[] { "a", "b", "c" });
+            Assert.AreEqual(Util.findDuplicateName(listb).Count, 0);
+        }
+    }
+}
