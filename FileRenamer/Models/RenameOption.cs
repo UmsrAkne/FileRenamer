39fd91fe318c03e0a6ae16f0fdaf448d09861653
Akn <sibata486@gmail.com>
2021-02-06 16:06:47 +0900

    クラス追加 / リネームの詳細設定を記録、設定するクラスを追加

---
+++ FileRenamer/Models/RenameOption.cs
@@ -0,0 +1,39 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRenamer.Models {
+    public class RenameOption : BindableBase{
+
+        public string AttachmentString {
+            get => attachmentString; set => SetProperty(ref attachmentString, value);
+        }
+
+        private string attachmentString = "";
+
+
+        public int InsertIndex {
+            get => insertIndex; set => SetProperty(ref insertIndex, value);
+        }
+
+        private int insertIndex = 0;
+
+
+        public int Digits {
+            get => digits; set => SetProperty(ref digits, value);
+        }
+
+        private int digits = 1;
+
+
+        public int StartCount {
+            get => startCount; set => SetProperty(ref startCount, value);
+        }
+
+        private int startCount = 0;
+
+    }
+}
